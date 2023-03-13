ca8907a42be47da48421babf23dc37a5794211e8
vlkvak <ksavlkva@gmail.com>
2023-03-13 03:57:43 +0300

    Leson 12

---
+++ modul12.py
@@ -0,0 +1,8 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Введите сумму"))
+deposit = []
+for key in per_cent:
+    deposit.append(per_cent[key] * money / 100)
+print(deposit)
+max_deposit = max(deposit)
+print("Максимальная сумма, которую вы можете заработать: " + str(max_deposit))
